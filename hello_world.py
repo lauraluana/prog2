0faf7db5b18adbbdb4f80991a6c4a7353bc99ef0
lauraluana <91331893+lauraluana@users.noreply.github.com>
2021-11-05 16:35:13 +0100

    second commit

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,16 @@
-print("Hello World")
+from flask import Flask
+from flask import render_template
+
+app = Flask("Hello World")
+
+
+@app.route("/hello")
+def hello():
+    return render_template('index.html', name="Noemi")
+
+@app.route("/test")
+def test():
+    return "passt!"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
