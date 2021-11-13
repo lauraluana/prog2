ec6b110b40dea1c9e4c38d744f213290f9e91a81
lauraluana <91331893+lauraluana@users.noreply.github.com>
2021-11-13 23:50:40 +0100

    bootstrap

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask("Hello World")
 
 
-@app.route("/hello")
-def hello():
-    return render_template('index.html', name="Noemi")
+@app.route('/hello/')
+def finanzen():
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
