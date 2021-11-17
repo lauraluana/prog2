716fcdda20eaea1a5df40d871b823f068b8457e7
lauraluana <91331893+lauraluana@users.noreply.github.com>
2021-11-17 17:01:11 +0100

    form built

---
+++ daten.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+import json
+
+
+def speichern(datei, key, value):
+    try:
+        with open(datei) as open_file:
+            datei_inhalt = json.load(open_file)
+    except FileNotFoundError:
+        datei_inhalt = {}
+
+    datei_inhalt[str(key)] = value
+
+    # print(datei_inhalt)
+
+    with open(datei, "w") as open_file:
+        json.dump(datei_inhalt, open_file, indent=4)
+
+
+def ausgaben_speichern(ausgaben):
+    datei_name = "ausgaben_1.json"
+    zeitpunkt = datetime.now()
+    speichern(datei_name, zeitpunkt, ausgaben)
+    return zeitpunkt, ausgaben
+
+
+def ausgaben_laden():
+    datei_name = "ausgaben_1.json"
+
+    try:
+        with open(datei_name) as open_file:
+            datei_inhalt = json.load(open_file)
+    except FileNotFoundError:
+        datei_inhalt = {}
+
+    return datei_inhalt
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from flask import request
+import daten
 
 app = Flask("Hello World")
 
@@ -10,5 +10,35 @@ def finanzen():
     return render_template('index.html')
 
 
-if __name__ == "__main__":
+@app.route("/speichern/<ausgaben>")
+def speichern(ausgaben):
+    zeitpunkt, ausgaben = daten.ausgaben_speichern(ausgaben)
+
+    return "Gespeichert: " + ausgaben + " um " + str(zeitpunkt)
+
+
+@app.route("/liste")
+def auflisten():
+    ausgaben = daten.ausgaben_laden()
+
+    ausgaben_liste = ""
+    for key, value in ausgaben.items():
+        zeile = str(key) + ": " + value + "<br>"
+        ausgaben_liste += zeile
+
+    return ausgaben_liste
+
+def ausgaben_laden():
+    datei_name = "aktivitaeten.json"
+
+    try:
+        with open(datei_name) as open_file:
+            datei_inhalt = json.load(open_file)
+    except FileNotFoundError:
+        datei_inhalt = {}
+
+    return datei_inhalt
+
+
+if __name__ == '__main__':
     app.run(debug=True, port=5000)
